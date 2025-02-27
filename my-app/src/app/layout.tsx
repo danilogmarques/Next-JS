9988f52ebfae7d02c352fdbe4d7f33d041f1648a
danilogmarques <danilogustavomarques@gmail.com>
2025-02-27 15:10:49 -0300

    iniciando server components x client components

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,5 +1,26 @@
 import "./globals.css";
 import { Header } from "../components/header"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title : "Home - Aula Next JS do zero!",
+  description : "Aprendendo Next JS do zero com sujeito programador!",
+  openGraph : {
+    title: "Aprendendo Next JS !",
+    description: "Dando meus próximos passos na carreira de desenvolvedor",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true, 
+      follow: true,
+    }
+  }
+
+}
+
 
 
 
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title : "Home - Aula Next JS do zero!",
+  description : "Aprendendo Next JS do zero com sujeito programador!",
+  openGraph : {
+    title: "Aprendendo Next JS !",
+    description: "Dando meus próximos passos na carreira de desenvolvedor",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true, 
+      follow: true,
+    }
+  }
+
+}
+
 export default function Home(){
   return(
     <div>
